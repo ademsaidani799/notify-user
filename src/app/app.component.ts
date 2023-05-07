db7e5c6da4cd321d27e631c04ca6f1aa8ee19206
adem saidani <ademsaidani799@gmail.com>
2023-05-07 12:05:56 +0200

    adding sweetAlert and testing Swal's fire functions

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import Swal from 'sweetalert2'
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'NotifyUser';
+  onSuccess(){
+    Swal.fire("Awesome","you have completed the course.","success")
+
+  }
+  onError(){
+    Swal.fire("Oops","An error occured.","error")
+
+  }
+}
